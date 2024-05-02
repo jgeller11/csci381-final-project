6c563875268c0976e3cd5b0a0440ea23c96cbcce
jgeller11 <34071775+jgeller11@users.noreply.github.com>
2024-05-01 23:22:34 -0400

    created noise generator class

---
+++ noiser.py
@@ -0,0 +1,17 @@
+import torch
+
+
+class Noiser():
+
+    # L: number of noise values to return per sample
+    # batch_size: number of samples to return
+    # seed: set to True to get controlled noise
+    def __init__(self, L, batch_size):
+        self.L = L
+        self.batch_size = batch_size
+        
+    # returns: uniform random values 0 to 1 in specified shape
+    def noise(self):
+        samples = torch.empty(self.batch_size, self.L)
+        torch.nn.init.uniform_(samples, a=0, b=1, generator=None)
+        return samples
